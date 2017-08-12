60c54688fe46bcf91faa89dd7bdc429035c92074
MarcoChrist <MarcoCh@hotmail.de>
2017-08-12 19:18:39 +0200

    Updated the Main.kt
    @nicolaiparlog
    The scneario function is now independent from CSV and the static list.

--- src/main/kotlin/FinalProject/Main.kt
+++ src/main/kotlin/FinalProject/Main.kt
@@ -10,7 +10,21 @@ import com.univocity.parsers.csv.CsvWriterSettings
  * simulates the traffic and prints the jammed / non-jammed for every car.
  */
 fun scenario(Cars: MutableList<Car>) {
-	/*val car1 = Car(true)
+
+	val rn = RoadNetwork(7, Cars)
+	rn.simulationStep()
+
+}
+fun scenarioCSV() {
+	val cars: MutableList<Car> = parseCSV()
+
+	scenario(cars)
+
+	printResultsToCSV(cars)
+
+}
+fun scenarioStatic(){
+	val car1 = Car(true)
 	val car2 = Car(false)
 	val car3 = Car(true)
 	val car4 = Car(true)
@@ -21,20 +35,22 @@ fun scenario(Cars: MutableList<Car>) {
 	val car9 = Car(true)
 	val car10 = Car(true)
 	val car11 = Car(false)
-	val car12 = Car(true) */
-	
-	//val Cars = listOf(car1, car2, car3, car4, car5, car6, car7, car8, car8, car9, car10, car11, car12)
-	val rn = RoadNetwork(7, Cars)
-	rn.simulationStep()
-	rn.printCarsJammed()
+	val car12 = Car(true)
+
+	val cars = mutableListOf(car1, car2, car3, car4, car5, car6, car7, car8, car8, car9, car10, car11, car12)
+	scenario(cars)
+
+
+		for (car in cars) {
+			println(car.getJammed())
+		}
+
 }
-
 fun main(args: Array<String>) {
-	val Cars : MutableList<Car> = parseCSV()
 
-	scenario(Cars)
+	scenarioCSV()
 
-	printResultsToCSV(Cars)
+	scenarioStatic()
 
 
 
@@ -42,7 +58,7 @@ fun main(args: Array<String>) {
 
 
 fun parseCSV(): MutableList<Car> {
-	val CarListCSV: MutableList<Car> = mutableListOf()
+	val carListCSV: MutableList<Car> = mutableListOf()
 
 	val settings = CsvParserSettings()
 	settings.format.setLineSeparator("\n")
@@ -54,16 +70,14 @@ fun parseCSV(): MutableList<Car> {
 
 
 	for (record in allRows){
-		val CarIdStr : String = record.values[0]
-		val CarId : Int = CarIdStr.toInt()
 
-		val WantDriveStr : String = record.values[1]
-		val WantDrive : Boolean = WantDriveStr.toBoolean()
+		val wantDriveStr : String = record.values[1]
+		val wantDrive : Boolean = wantDriveStr.toBoolean()
 
-		CarListCSV.add(Car(d = WantDrive))
+		carListCSV.add(Car(d = wantDrive))
 
 	}
-	return CarListCSV
+	return carListCSV
 }
 
 fun printResultsToCSV(results: List<Car>, outputFile: String = "results.csv") {
@@ -76,16 +90,16 @@ fun printResultsToCSV(results: List<Car>, outputFile: String = "results.csv") {
 	// Write the record headers of this file
 	val vehicleRows: MutableList<Array<Any>> = mutableListOf()
 
-	var Drive = "I want to drive"
-	var Jammed = "In a traffic jam"
-	var row: Array<Any> = arrayOf(Drive, Jammed)
+	var drive = "I want to drive"
+	var jammed = "In a traffic jam"
+	var row: Array<Any> = arrayOf(drive, jammed)
 	vehicleRows.add(row)
 
 	for (result in results) {
 
-		Drive = result.getDrive().toString()
-		Jammed = result.getJammed().toString()
-		row = arrayOf(Drive, Jammed)
+		drive = result.getDrive().toString()
+		jammed = result.getJammed().toString()
+		row = arrayOf(drive, jammed)
 		vehicleRows.add(row)
 	}
 
